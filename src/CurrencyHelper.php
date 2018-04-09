52e34ab482d7053bcdac7b3b3c9ebe92382b9d93
Valentino Međimorec <valentino@medjimorec.net>
2018-04-09 09:48:27 +0200

    Helper function for building admin form. Added admin settings form

---
+++ src/CurrencyHelper.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Drupal\commerce_currency_resolver;
+
+use Drupal\commerce_price\Calculator;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+
+/**
+ * Class CurrencyHelper.
+ *
+ * @package Drupal\commerce_currency_resolver
+ */
+class CurrencyHelper {
+
+  /**
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface;
+   */
+  protected $entityTypeManager;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(ConfigFactoryInterface $configFactory, EntityTypeManagerInterface $entityTypeManager) {
+    $this->configFactory = $configFactory;
+    $this->entityTypeManager = $entityTypeManager;
+  }
+
+  /**
+   * Get all available option where currency could be mapped.
+   *
+   * @return array
+   *   Array of options available for currency mapping.
+   */
+  public static function getAvailableMapping() {
+    $mapping = [];
+
+    // Default none.
+    $mapping['0'] = t('None');
+
+    // Check if exist geo based modules.
+    // We support for now two of them.
+    if (\Drupal::ModuleHandler()->moduleExists('smart_ip') || \Drupal::ModuleHandler()->moduleExists('geoip')) {
+      $mapping['geo'] = t('By Country');
+    }
+
+    // Check if we site is multilingual.
+    if (\Drupal::languageManager()->isMultilingual()) {
+      $mapping['lang'] = t('By Language');
+    }
+
+    return $mapping;
+  }
+
+  /**
+   * Get list of all enabled languages.
+   *
+   * @return array
+   *   Array of languages.
+   */
+  public static function getAvailableLanguages() {
+    $languages = \Drupal::languageManager()->getLanguages();
+
+    $data = [];
+
+    foreach ($languages as $key => $lang) {
+      $data[$key] = $lang->getName();
+    }
+
+    return $data;
+  }
+
+  /**
+   * Get list of all enabled currencies.
+   *
+   * @return array
+   *   Array of currencies.
+   */
+  public static function getEnabledCurrency() {
+    $enabled_currencies = \Drupal::EntityTypeManager()
+      ->getStorage('commerce_currency')
+      ->loadByProperties([
+        'status' => TRUE,
+      ]);
+
+    $currencies = [];
+
+    foreach ($enabled_currencies as $key => $currency) {
+      $currencies[$key] = $currency->getName();
+    }
+
+    return $currencies;
+  }
+
+  public function getDefaultCurrency() {
+    $enabled_currencies = $this->entityTypeManager->getStorage('commerce_currency')
+      ->loadByProperties([
+        'status' => TRUE,
+      ]);
+
+    if (isset($enabled_currencies['USD'])) {
+      return $enabled_currencies['USD'];
+    }
+    else {
+      return reset($enabled_currencies);
+    }
+  }
+}
--- src/Form/CommerceCurrencyResolverForm.php
+++ src/Form/CommerceCurrencyResolverForm.php
@@ -4,6 +4,7 @@ namespace Drupal\commerce_currency_resolver\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\commerce_currency_resolver\CurrencyHelper;
 
 /**
  * Class CommerceMulticurrencySettingsForm.
@@ -11,7 +12,6 @@ use Drupal\Core\Form\FormStateInterface;
  * @package Drupal\commerce_currency_resolver\Form
  */
 class CommerceCurrencyResolverForm extends ConfigFormBase {
-
 
   /**
    * {@inheritdoc}
@@ -32,47 +32,95 @@ class CommerceCurrencyResolverForm extends ConfigFormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
 
+    // Get current settings.
     $config = $this->config('commerce_currency_resolver.settings');
 
-    $form['currency_mapping'] = [
+    $form['info'] = [
+      '#type' => 'details',
+      '#title' => t('Currency settings'),
+      '#open' => TRUE,
+    ];
+
+    $form['info']['currency_mapping'] = [
       '#type' => 'radios',
       '#title' => $this->t('Currency mapping'),
-      '#description' => $this->t('Select which logic currency is mapped'),
-      '#options' => array(
-        'lang' => $this->t('By Language'),
-        'geo' => $this->t('By Country'),
-      ),
+      '#description' => $this->t('Select how currency is mapped in the system. By country, language.'),
+      '#options' => CurrencyHelper::getAvailableMapping(),
       '#default_value' => $config->get('currency_mapping'),
       '#required' => TRUE,
     ];
 
-    $form['currency_source'] = [
+    $form['info']['currency_source'] = [
       '#type' => 'radios',
       '#title' => $this->t('Currency source'),
       '#description' => $this->t('Select how currency is added and calculated. To avoid possible errors, "Combo mode" is best option. If field for currency does not exist, it will fallback to automatic conversion for this specific currency'),
-      '#options' => array(
+      '#options' => [
         'auto' => $this->t('Automatic conversion'),
         'fields' => $this->t('Price field per currency'),
         'combo' => $this->t('Combo mode'),
-      ),
+      ],
       '#default_value' => $config->get('currency_source'),
       '#required' => TRUE,
     ];
 
-    $active_currencies = \Drupal::state()->get('active_currencies');
+    $form['currency'] = [
+      '#type' => 'details',
+      '#title' => t('Currency conversion'),
+      '#open' => FALSE,
+    ];
 
-    $form['currency_conversion'] = [
+    $form['currency']['conversion'] = [
       '#type' => 'select',
-      '#title' => $this->t('Currency conversion'),
-      '#description' => $this->t('Select how currency is calculated'),
-      '#options' => array(
+      '#title' => $this->t('3rd party exchange service'),
+      '#description' => $this->t('Select which external service you want to use for calucalting exchange rates between currencies'),
+      '#options' => [
         'manual' => $this->t('Manual'),
         'external' => $this->t('External'),
         'combo' => $this->t('Combination'),
-      ),
+      ],
       '#default_value' => $config->get('currency_conversion'),
       '#required' => TRUE,
     ];
+
+    $form['mapping'] = [
+      '#type' => 'details',
+      '#title' => t('Currency mapping'),
+      '#open' => FALSE,
+    ];
+
+    $active_currencies = CurrencyHelper::getEnabledCurrency();
+
+    // Render options in form.
+    foreach ($active_currencies as $key => $item) {
+
+      $form['currency'][$key]['value'] = [
+        '#type' => 'textfield',
+        '#title' => t('List countries which should use following currency: @currency', ['@currency' => $item]),
+      ];
+
+      $form['currency'][$key]['auto'] = [
+        '#type' => 'checkboxes',
+        '#title' => t('List countries which should use following currency: @currency', ['@currency' => $item]),
+        '#options' => [1 => 'Auto']
+      ];
+
+      switch ($config->get('currency_mapping')) {
+        case 'lang':
+          $form['mapping'][$key] = [
+            '#type' => 'select',
+            '#options' => CurrencyHelper::getAvailableLanguages(),
+            '#title' => t('Choose language for @currency', ['@currency' => $item]),
+          ];
+          break;
+
+        case 'geo':
+          $form['mapping'][$key] = [
+            '#type' => 'textfield',
+            '#title' => t('List countries which should use following currency: @currency', ['@currency' => $item]),
+          ];
+          break;
+      }
+    }
 
     $form['submit'] = [
       '#type' => 'submit',
@@ -93,11 +141,14 @@ class CommerceCurrencyResolverForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    // Display result.
-    foreach ($form_state->getValues() as $key => $value) {
-      drupal_set_message($key . ': ' . $value);
-    }
+    $config = $this->config('commerce_currency_resolver.settings');
 
+    // Set values.
+    $config->set('currency_mapping', $form_state->getValue('currency_mapping'))
+      ->set('currency_source', $form_state->getValue('currency_source'))
+      ->save();
+
+    parent::submitForm($form, $form_state);
   }
 
 }
