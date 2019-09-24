028b6e3e905d2fae12d3969cf35b5d6d162b51c7
Valentino Međimorec <valentino@medjimorec.net>
2019-09-24 09:59:29 +0200

    Block currency selector - inject form builder

--- src/Plugin/Block/CommerceCurrencyResolverSelectBlock.php
+++ src/Plugin/Block/CommerceCurrencyResolverSelectBlock.php
@@ -4,6 +4,8 @@ namespace Drupal\commerce_currency_resolver\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
 use Drupal\commerce_currency_resolver\Form\CommerceCurrencyResolverSelectForm;
+use Drupal\Core\Form\FormBuilderInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides Commerce currency block.
@@ -17,16 +19,37 @@ use Drupal\commerce_currency_resolver\Form\CommerceCurrencyResolverSelectForm;
 class CommerceCurrencyResolverSelectBlock extends BlockBase {
 
   /**
+   * FormBuilderInterface.
+   *
+   * @var \Drupal\Core\Form\FormBuilderInterface
+   */
+  protected $formBuilder;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, FormBuilderInterface $form_builder) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->formBuilder = $form_builder;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('form_builder')
+    );
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function build() {
-
-    $markup = [];
-
-    $form = \Drupal::formBuilder()->getForm(CommerceCurrencyResolverSelectForm::class);
-
-    $markup['form'] = $form;
-
+    $markup['form'] = $this->formBuilder->getForm(CommerceCurrencyResolverSelectForm::class);
     return $markup;
   }
 
